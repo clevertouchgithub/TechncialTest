0f3e995397021a7e5f837b2535ce30a3c54d021e
EC2 Default User <ec2-user@ip-172-31-38-147.eu-west-1.compute.internal>
2021-04-27 11:44:10 +0000

    Added carousel js code snippet. Improved UI.

--- static/js/custom.js
+++ static/js/custom.js
@@ -369,4 +369,30 @@ $(function () {
 	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
 	$('#copyright-years').text(+ new Date().getFullYear());
 
+	/* Carousel 
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	$('#myCarousel').carousel({
+            interval: false
+        });
+
+        //scroll slides on swipe for touch enabled devices
+
+        $("#myCarousel").on("touchstart", function(event){
+
+            var yClick = event.originalEvent.touches[0].pageY;
+            $(this).one("touchmove", function(event){
+
+                var yMove = event.originalEvent.touches[0].pageY;
+                if( Math.floor(yClick - yMove) > 1 ){
+                    $(".carousel").carousel('next');
+                }
+                else if( Math.floor(yClick - yMove) < -1 ){
+                    $(".carousel").carousel('prev');
+                }
+            });
+            $(".carousel").on("touchend", function(){
+                $(this).off("touchmove");
+            });
+        });
+
 });
